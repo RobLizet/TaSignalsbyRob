da27de642b203536d289ecab973b7ab0a722e7f0
RobLizet <zweetzakken@gmail.com>
2026-04-12 12:09:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,27 @@
-const CACHE = 'signals-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = ‘signals-v2’;
+const FILES = [
+‘./index.html’,
+‘./manifest.json’
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener(‘install’, e => {
+e.waitUntil(
+caches.open(CACHE).then(c => c.addAll(FILES))
+);
+self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener(‘activate’, e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('api.anthropic') || e.request.url.includes('fonts.googleapis')) {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }))
-    );
-  }
+self.addEventListener(‘fetch’, e => {
+e.respondWith(
+fetch(e.request).catch(() => caches.match(e.request))
+);
 });
