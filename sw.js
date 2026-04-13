008a7213b00eb25434caaa413c384c80d2ab92e0
RobLizet <zweetzakken@gmail.com>
2026-04-13 10:01:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'signals-v1';
-const ASSETS = ['/', '/index.html'];
+const ASSETS = ['/TaSignalsbyRob/', '/TaSignalsbyRob/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
